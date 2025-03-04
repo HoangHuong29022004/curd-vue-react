203cbbcda61276cd280aa6d9b01d2e745eef156e
Hoang Huong | apikey:d0851193d83d8c0dc0833e050f2c432f59dd585145eb42c2440363789246f383 <huong.ph@fsofts.com>
2025-03-04 08:38:19 +0700

    save

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,4 +4,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import router from "./routers";
 
-createApp(App).use(router).mount("#app");
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+createApp(App)
+  .use(router)
+  .use(vuetify)
+  .mount("#app");
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,21 +2,24 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vuetify from '@vuetify/vite-plugin'
+import vuetify from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vuetify()],
+  plugins: [
+    vue(),
+    vuetify({ autoImport: true }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }, // <-- Missing closing bracket added here
+  },
   server: {
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:5000/",
+        target: "http://localhost:5001/",
         changeOrigin: true,
       },
     }
